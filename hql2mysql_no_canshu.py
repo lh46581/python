fb1a696a064d27c13423dbaa7c7edf2fc8f524bf
lh <liuhui199805@163.com>
2022-02-24 15:28:07 +0800

    删除

--- hql2mysql_no_canshu.py
+++ hql2mysql_no_canshu.py
@@ -13,7 +13,7 @@ for number in range(len(strList)):
     strSpilt = strList[number].split()  # 拆分成：`patient_id`  bigint     COMMENT    '患者id',
 
     if number == 0:
-        headStr0 = strSpilt[-1].split(".")  # 切分成`shuidi_sdm  sdm_cro_patient_project_rel_opt_log_full_d`(
+        headStr0 = strSpilt[-1].split(".")  # 切分成`  sdm_cro_patient_project_rel_opt_log_full_d`(
         headStr1 = headStr0[1][:-2]
         headStr2 = "CREATE TABLE " + "`" + headStr1 + "`" + "("
         print(headStr2)
